d6b6f234033a0066ef0c62fa35de45c9970297a8
Spencer McDonald <mcdonaldspence@hotmail.com>
2018-03-08 15:35:41 -0800

    Renamed ViewController to NewTripViewController and changed configuration to portrait only mode

--- travely/NewTripViewController.swift
+++ travely/NewTripViewController.swift
@@ -1,32 +1,29 @@
 //
-//  ViewController.swift
+//  NewTripViewController.swift
 //  travely
 //
 //  Created by Spencer Mcdonald on 3/3/18.
 //  Copyright © 2018 University of San Diego. All rights reserved.
 //
+// View Controller for New Trip (Main Screen) tab
 
 import UIKit
 
-class ViewController: UIViewController {
+class NewTripViewController: UIViewController {
 
+    @IBOutlet weak var departureDatePicker: UIDatePicker!
+    @IBOutlet weak var returnDatePicker: UIDatePicker!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
-
-    //Testing for sprint0
-
-    
-    
-    
-    //random test comment
-
-
 }
 
