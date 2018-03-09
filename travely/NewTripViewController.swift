548de4e3b576905883637a5ec251814cf5ca0f07
Spencer McDonald <mcdonaldspence@hotmail.com>
2018-03-08 17:57:59 -0800

    Added date picker date validation before the calculate button is pressed

--- travely/NewTripViewController.swift
+++ travely/NewTripViewController.swift
@@ -25,5 +25,34 @@ class NewTripViewController: UIViewController {
         // Dispose of any resources that can be recreated.
         
     }
+    
+    //If calculate button is pressed then initiate action
+    @IBAction func calculateButtonPressed(_ sender: UIButton) {
+        
+        //Get time values and change format of dates
+        let current_date = Date()
+        let departure_date = departureDatePicker.date
+        let return_date = returnDatePicker.date
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        let departure_date_str: String = formatter.string(from: departure_date)
+        let return_date_str: String = formatter.string(from: return_date)
+        let current_date_str: String = formatter.string(from: current_date)
+        
+        //Check that departure date is not less than the current date
+        guard departure_date_str >= current_date_str else {
+            print("Invalid Dates: departure time less than current date")
+            return
+        }
+        
+        //Check that departure date is not greater than return date
+        guard departure_date_str <= return_date_str else {
+            print("Invalid Dates: departure time is greater than return date")
+            return
+        }
+        
+        //TODO: Implement calculate button function
+    }
 }
 
