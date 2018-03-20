169e18358b9f844cdc0fcdb45dcdcf46317d1290
Matt <matt@macbook-pro.local>
2018-03-20 09:56:42 -0700

    Added a new file, SavedTripDetailsView

---
+++ travely/SavedTripDetailsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SavedTripDetailsViewController.swift
+//  travely
+//
+//  Created by Matt on 3/20/18.
+//  Copyright © 2018 University of San Diego. All rights reserved.
+//
+
+import Foundation
