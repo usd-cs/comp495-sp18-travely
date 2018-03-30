bcd692337e83051b2395cdec5cac513537b9ba49
Saul Garza <saulgarza@Sauls-MacBook-Air.local>
2018-03-30 12:56:28 -0700

    added UI elements to about tab for currency calculator

--- travely/AboutViewController.swift
+++ travely/AboutViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class AboutViewController: UIViewController {
-
+class AboutViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    
+    let FIXER_BASE_URL = "http://data.fixer.io/api/"
+    
+    let rates = ["USD", "EUR", "MXN","GBP"]
+    
+    @IBOutlet weak var exchangeResult: UILabel!
+    @IBOutlet weak var exchangeAmt: UITextField!
+    @IBOutlet weak var currCurrency: UIPickerView!
+    @IBOutlet weak var destinationCurrency: UIPickerView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +30,28 @@ class AboutViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return rates.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return rates[row]
+    }
+    
+    //when the button is pressed, this will check for errors, then make the api call
+    @IBAction func calcExchangeRate(_ sender: UIButton) {
+        if let amount = Int(exchangeAmt.text!){
+            print(amount)
+        }
+        else{
+            print("Invalid Amount. Please try again.")
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
