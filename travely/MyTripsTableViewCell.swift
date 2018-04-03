ec9a8c5111229ba811e3ff5a7e93d13c16256b1d
Spencer McDonald <mcdonaldspence@hotmail.com>
2018-04-02 18:01:29 -0700

    Added File for MyTripsTableViewCell

---
+++ travely/MyTripsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyTripsTableViewCell.swift
+//  travely
+//
+//  Created by Spencer Mcdonald on 4/2/18.
+//  Copyright © 2018 University of San Diego. All rights reserved.
+//
+
+import UIKit
+
+class MyTripsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
