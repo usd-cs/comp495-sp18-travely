9139430225344e847d18a091f84992f9662baaf6
Spencer McDonald <mcdonaldspence@hotmail.com>
2018-04-03 13:23:20 -0700

    Added new variables to Trip file

--- travely/Trip.swift
+++ travely/Trip.swift
@@ -10,12 +10,31 @@ import UIKit
 
 class Trip {
     var tripName: String
+    var tripTotalCost: Double
+    var tripAirfareCost: Double
+    var tripHotelCost: Double
+    var foodCost: Double
+    var activitiesCost: Double
+    var originLocation: String
+    var destinationLocation: String
+    var departureDate: String
+    var returnDate: String
     
-    init?(tripName: String){
-        if tripName.isEmpty {
+    init?(tripName: String, tripTotalCost: Double, tripAirfareCost: Double, tripHotelCost: Double, foodCost: Double, activitiesCost: Double, originLocation: String, destinationLocation: String, departureDate: String, returnDate: String){
+        
+        if tripName.isEmpty || originLocation.isEmpty || destinationLocation.isEmpty || departureDate.isEmpty || returnDate.isEmpty {
             return nil
         }
         
         self.tripName = tripName
+        self.tripTotalCost = tripTotalCost
+        self.tripAirfareCost = tripAirfareCost
+        self.tripHotelCost = tripHotelCost
+        self.foodCost = foodCost
+        self.activitiesCost = activitiesCost
+        self.originLocation = originLocation
+        self.destinationLocation = destinationLocation
+        self.departureDate = departureDate
+        self.returnDate = returnDate
     }
 }
