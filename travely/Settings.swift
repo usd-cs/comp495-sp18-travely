91e4a315560cc63353ba065c232e3a799b53c62f
alexandraleonidova <alexandraleonidova@sandiego.edu>
2018-04-22 19:03:56 -0700

    created settings class

---
+++ travely/Settings.swift
@@ -0,0 +1,13 @@
+//
+//  Settings.swift
+//  travely
+//
+//  Created by Alexandra Leonidova on 4/22/18.
+//  Copyright © 2018 University of San Diego. All rights reserved.
+//
+
+import Foundation
+
+struct Settings{
+    
+}
